3716ad95fa597d70a4c12654acc108121f8dd026
Yevhenii Orenchuk <orenchuk98@gmail.com>
2017-11-27 18:27:40 +0200

    deleted all not necessary files

--- Debug/Drivers/STM32F3Discovery/Src/lsm303dlhc.d
+++
@@ -1,8 +0,0 @@
-Drivers/STM32F3Discovery/Src/lsm303dlhc.o: \
- ../Drivers/STM32F3Discovery/Src/lsm303dlhc.c \
- /Users/orenchuk/eclipse-workspace/AccelerometerV1.0/Drivers/STM32F3Discovery/Inc/lsm303dlhc.h \
- /Users/orenchuk/eclipse-workspace/AccelerometerV1.0/Drivers/STM32F3Discovery/Inc/Common/accelero.h
-
-/Users/orenchuk/eclipse-workspace/AccelerometerV1.0/Drivers/STM32F3Discovery/Inc/lsm303dlhc.h:
-
-/Users/orenchuk/eclipse-workspace/AccelerometerV1.0/Drivers/STM32F3Discovery/Inc/Common/accelero.h:
